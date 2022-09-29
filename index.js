d7430254bde1f3e0602d31f1fb357d7b54226010
Harsh Pansuriya <harshpansuriya123@gmail.com>
2022-09-29 10:42:12 -0400

    Complete initial code for config file support feature

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const { Command } = require('commander');
+const { Command } = require("commander");
 const program = new Command();
-const fs = require('fs');
-const htmlCreator = require('html-creator');
+const fs = require("fs");
+const htmlCreator = require("html-creator");
 var filePaths = []; //keep track of .txt files converted
-var outputPath = './dist'
-/** 
-*  Create htmlCreator object using 2 params
-*  @param: paragraphObj, an object of {type, content} for <p>, for .md file paragraphObj is body object containing more than <p>, <a>
-*  @return: an object of type htmlCreator, can use htmlRender() to convert to string
-*/
+var outputPath = "./dist";
+/**
+ *  Create htmlCreator object using 2 params
+ *  @param: paragraphObj, an object of {type, content} for <p>, for .md file paragraphObj is body object containing more than <p>, <a>
+ *  @return: an object of type htmlCreator, can use htmlRender() to convert to string
+ */
 const createHtml = (paragraphObj, titleObj) => {
   const html = new htmlCreator().withBoilerplate();
-  var bodyContent = [{
-    type: 'div',
-    attributes: {className: 'paragraphObj'},
-    content: paragraphObj
-  }]
+  var bodyContent = [
+    {
+      type: "div",
+      attributes: { className: "paragraphObj" },
+      content: paragraphObj,
+    },
+  ];
   // if a title is found, add the title wrapped inside `<h1>`
   // tag to the top of the `<body>` HTML element
   if (titleObj.content) {
     bodyContent.unshift({
-      type: 'h1',
+      type: "h1",
       content: titleObj.content,
     });
   }
   if (paragraphObj == null) {
     bodyContent.pop();
-  } 
+  }
   // Append title to the `<head>` HTML element
   html.document.addElementToType("head", {
     type: "title",
@@ -40,143 +42,214 @@ const createHtml = (paragraphObj, titleObj) => {
       href: "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
     },
   });
-  html.document.addElementToType('body', bodyContent);
+  html.document.addElementToType("body", bodyContent);
   return html;
-}
+};
 
-/** 
-*  Look for title and convert text files into html files
-*  @param: filePath from commandLine
-*/
+/**
+ *  Look for title and convert text files into html files
+ *  @param: filePath from commandLine
+ */
 const createHtmlFiles = (filePath, fileType) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if(err)
-      return console.error(`Unable to read file ${filePath}`, err);
-    
-    let htmlTitle = null; 
-    let titleObj = new Object({ type: 'title', content: htmlTitle });
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) return console.error(`Unable to read file ${filePath}`, err);
+
+    let htmlTitle = null;
+    let titleObj = new Object({ type: "title", content: htmlTitle });
     //check for title, regEx checks if a line is followed by 2 newline \n\n\n
-    if(data.match(/^.+(\r?\n\r?\n\r?\n)/)) {
-      htmlTitle = data.match(/^.+(\r?\n\r?\n\r?\n)/)[0]; 
-      titleObj['content'] = htmlTitle.match(/(\w+)/g).join(' ');
+    if (data.match(/^.+(\r?\n\r?\n\r?\n)/)) {
+      htmlTitle = data.match(/^.+(\r?\n\r?\n\r?\n)/)[0];
+      titleObj["content"] = htmlTitle.match(/(\w+)/g).join(" ");
     }
 
     const paragraphObj = data
       .substr(htmlTitle ? htmlTitle.length : 0)
       .split(/\r?\n\r?\n/)
-      .map(param => {
+      .map((param) => {
         if (fileType == "md") {
-          return markdownToHtml(param)
+          return markdownToHtml(param);
         } else {
-          return Object({ type: 'p', content: param});
+          return Object({ type: "p", content: param });
         }
       });
 
     const fileToHtml = createHtml(paragraphObj, titleObj);
-    const fullFilePath = `${outputPath}/${filePath.match(/([^\/]+$)/g)[0].split('.')[0]}.html`; 
+    const fullFilePath = `${outputPath}/${
+      filePath.match(/([^\/]+$)/g)[0].split(".")[0]
+    }.html`;
     fs.writeFile(fullFilePath, fileToHtml.renderHTML(), (err) => {
-      if(err)
-        return console.error(`Unable to write file ${fullFilePath} `, err); 
+      if (err)
+        return console.error(`Unable to write file ${fullFilePath} `, err);
       console.log(`${fullFilePath} is created!`);
     });
   });
   filePaths.push(filePath);
-}
+};
 
 const markdownToHtml = (param) => {
   // If Heading 1 to 6, turn into corresponding h1 to h6 tag
   if (param.match(/^\s*#{1,6}[^#]+$/)) {
-    const headerNum = param.match(/#/g).length
-    return Object({ type: `h${headerNum}`, content: param.replace(/^\s*#{1,6}([^#]+)$/, "$1")});
-  }
-  else {
+    const headerNum = param.match(/#/g).length;
+    return Object({
+      type: `h${headerNum}`,
+      content: param.replace(/^\s*#{1,6}([^#]+)$/, "$1"),
+    });
+  } else {
     // Wrap bold text inside <b></b>
-    param = param.replace(/\*\*([^\*]+)\*\*/g, "<b>$1</b>")
-    param = param.replace(/__([^\*]+)__/g, "<b>$1</b>")
+    param = param.replace(/\*\*([^\*]+)\*\*/g, "<b>$1</b>");
+    param = param.replace(/__([^\*]+)__/g, "<b>$1</b>");
 
     // Wrap italic text inside <i></i>
-    param = param.replace(/\*([^\*]+)\*/g, "<i>$1</i>")
-    param = param.replace(/_([^\*]+)_/g, "<i>$1</i>")
+    param = param.replace(/\*([^\*]+)\*/g, "<i>$1</i>");
+    param = param.replace(/_([^\*]+)_/g, "<i>$1</i>");
 
     //Wrap code inside <code>
     param = param.replace(/\`([^\`].+?)\`/g, "<code>$1</code>");
-    param = param.replace(/(```([^`].+?)```)/gms, "<code>$2</code>")
+    param = param.replace(/(```([^`].+?)```)/gms, "<code>$2</code>");
 
     // Turn link: [Title](http://example.com) into: <a href="http://example.com">Title</a>
-    param = param.replace(/\[(.+)\]\((.+)\)/, '<a href="$2">$1</a>')
+    param = param.replace(/\[(.+)\]\((.+)\)/, '<a href="$2">$1</a>');
 
-    if(param.match(/\[(.+)\]\((.+)\)/))
-      return Object({type: 'a', attributes: {href: param.match(/\[(.+)\]\((.+)\)/)[2]}, content: param.match(/\[(.+)\]\((.+)\)/)[1]}); 
-    if(param.match(/---/))
-      return Object({type: 'hr', content: null});
-    return Object({ type: 'p', content: param});
+    if (param.match(/\[(.+)\]\((.+)\)/))
+      return Object({
+        type: "a",
+        attributes: { href: param.match(/\[(.+)\]\((.+)\)/)[2] },
+        content: param.match(/\[(.+)\]\((.+)\)/)[1],
+      });
+    if (param.match(/---/)) return Object({ type: "hr", content: null });
+    return Object({ type: "p", content: param });
   }
-}
+};
 
 /**
-*  Check if filePath is valid (folder or file .txt), if .txt file => call createHtmlFiles(filePath)
-*  @param: filePath from commandLine
-*  @param: isCheckPath, boolean for checking if the function is for checking output path
-*/
+ *  Check if filePath is valid (folder or file .txt), if .txt file => call createHtmlFiles(filePath)
+ *  @param: filePath from commandLine
+ *  @param: isCheckPath, boolean for checking if the function is for checking output path
+ */
 const readInput = (filePath) => {
-  const stat = fs.lstatSync(filePath); 
-  if(!stat.isFile() && stat.isDirectory()) {
+  const stat = fs.lstatSync(filePath);
+  if (!stat.isFile() && stat.isDirectory()) {
     fs.readdirSync(filePath).forEach((file) => {
-        //recursive until a .txt file is recognized
-        readInput(`${filePath}/${file}`);
-    })
-  }
-  else if(stat.isFile() && filePath.split('.').pop() == "txt") {
+      //recursive until a .txt file is recognized
+      readInput(`${filePath}/${file}`);
+    });
+  } else if (stat.isFile() && filePath.split(".").pop() == "txt") {
     createHtmlFiles(filePath, "txt");
-  }
-  else if (stat.isFile() && filePath.split(".").pop() == "md") {
+  } else if (stat.isFile() && filePath.split(".").pop() == "md") {
     createHtmlFiles(filePath, "md");
   }
-}
+};
 
-//configure program
-program.version('tue-1st-ssg 0.1', '-v, --version');
-program 
-  .option('-o, --output <path>', 'specify a path for .html files output')
-  .requiredOption('-i, --input <file path>', '(required) transform .txt or .md files into .html files');
-
-program.parse(process.argv)
-
-//Look for option
-const option = program.opts();
-if(option.output) {
-  let tempPath = fs.statSync(option.output); 
-  if(tempPath.isDirectory())
-    outputPath = option.output
-}
-
-if(option.input) {
-  if (!fs.existsSync(outputPath)) 
-    fs.mkdirSync(outputPath);
-  readInput(option.input);
+/**
+ *  Process input <filepath>
+ *  @param: filePath from commandLine
+ */
+const processInput = (filepath) => {
+  if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath);
+  readInput(filepath);
   //delete previous html files in the output folder after generating new html files
-  fs.readdirSync(outputPath).forEach(file => {
-    const outputFolderFile = `${outputPath}/${file}`
-    if(filePaths.indexOf(outputFolderFile) < 0 && outputFolderFile.split('.').pop() == "html") {
+  fs.readdirSync(outputPath).forEach((file) => {
+    const outputFolderFile = `${outputPath}/${file}`;
+    if (
+      filePaths.indexOf(outputFolderFile) < 0 &&
+      outputFolderFile.split(".").pop() == "html"
+    ) {
       fs.unlink(outputFolderFile, (err) => {
-        if(err) console.log(err);
-      })
+        if (err) console.log(err);
+      });
     }
   });
   //creating index.html linking all html files
-  const indexHtml = createHtml(null, {type: 'title', content: 'Index'});
-  const linkObj = filePaths.map(param => {
+  const indexHtml = createHtml(null, { type: "title", content: "Index" });
+  const linkObj = filePaths.map((param) => {
     return {
-      type: 'a', 
+      type: "a",
       //replace white space with %20
-      attributes: {href: `${param.match(/([^\/]+$)/g)[0].split('.')[0].replace(/\s/g, '%20')}.html`, style: 'display: block'}, 
-      content: `${param.match(/([^\/]+$)/g)[0].split('.')[0]}`
-    }
+      attributes: {
+        href: `${param
+          .match(/([^\/]+$)/g)[0]
+          .split(".")[0]
+          .replace(/\s/g, "%20")}.html`,
+        style: "display: block",
+      },
+      content: `${param.match(/([^\/]+$)/g)[0].split(".")[0]}`,
+    };
   });
-  indexHtml.document.addElementToType('body', { type: 'div', content: linkObj }) ;
-  fs.writeFile(`${outputPath}/index.html`, indexHtml.renderHTML(), (err) => {
-    if(err)
-      return console.error(`Unable to write files ${outputPath} `, err); 
-    console.log(`${outputPath}/index.html is created`);
-  });  
-} 
+  indexHtml.document.addElementToType("body", {
+    type: "div",
+    content: linkObj,
+  });
+  fs.writeFile(
+    `${outputPath}/index.html`,
+    indexHtml
+      .renderHTML()
+      .replace(
+        /<html>/,
+        `<html lang="${option.lang ? option.lang : defaultLang}">`
+      ),
+    (err) => {
+      if (err) return console.error(`Unable to create index.html file`, err);
+      console.log(`${outputPath}/index.html created`);
+    }
+  );
+};
+
+//configure program
+program.version("tue-1st-ssg 0.1", "-v, --version");
+program
+  .option("-o, --output <path>", "specify a path for .html files output")
+  .option(
+    "-i, --input <file path>",
+    "(required) transform .txt or .md files into .html files"
+  )
+  .option(
+    "-c, --config <config file path>",
+    "use stored commands in config file"
+  );
+program.parse(process.argv);
+
+//Look for option
+const option = program.opts();
+if (option.config) {
+  //if specified file doesn't exist
+  if (!fs.existsSync(option.config))
+    console.error(
+      `Config file "${option.config}" doesn't exist, please check file path.`
+    );
+  else {
+    //parse JSON contents
+    let rawData = fs.readFileSync(option.config);
+    let configOpts = JSON.parse(rawData);
+    //if the JSON file isn't empty
+    if (configOpts) {
+      if (configOpts.output && fs.existsSync(configOpts.output)) {
+        let tempPath = fs.statSync(configOpts.output);
+        if (tempPath.isDirectory()) outputPath = configOpts.output;
+      }
+
+      if (configOpts.input && fs.existsSync(configOpts.input)) {
+        option.lang = configOpts.lang;
+        processInput(configOpts.input);
+      } else if (!configOpts.input)
+        console.error(
+          `error: input '<file path>' not specified in config file`
+        );
+      else
+        console.error(
+          `error: no file or directory at input file path ${configOpts.input}, please check file path`
+        );
+    } else
+      console.error(`error: input '<file path>' not specified in config file`);
+  }
+} else {
+  if (option.output) {
+    let tempPath = fs.statSync(option.output);
+    if (tempPath.isDirectory()) outputPath = option.output;
+  }
+
+  if (option.input) processInput(option.input);
+  else
+    console.error(
+      `error: required option '-i, --input <file path>' not specified`
+    );
+}
